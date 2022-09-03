a1aff6ea8b52e07b27470e7d9f9c3b41f3a185af
JeanOSM <106047121+JeanOSM@users.noreply.github.com>
2022-09-03 14:46:42 -0300

    Update Curso de BD.sql

--- Curso de BD.sql
+++ Curso de BD.sql
@@ -1,4 +1,6 @@
- DROP DATABASE IF EXISTS aula_banco;
+#aula01
+
+DROP DATABASE IF EXISTS aula_banco;
  CREATE DATABASE aula_banco;
  USE aula_banco;
  DROP TABLE IF EXISTS estado;
@@ -16,3 +18,22 @@
  INSERT INTO estado (nome_estado, sigla) VALUES ( "SÃO PALO","SP");
  SELECT id,nome_estado,sigla,ativo,data_cadastro FROM estado;
  
+ 
+ #aula02
+ 
+ CREATE TABLE cidade (
+ id INT NOT NULL AUTO_INCREMENT
+ ,nome_cidade VARCHAR (200) NOT NULL
+ , ativo CHAR (1) DEFAULT 'S'
+ ,data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+ ,estado_id INT NOT NULL
+ ,CONSTRAINT pk_cidade PRIMARY KEY (id)
+ ,CONSTRAINT fk_estado FOREIGN KEY (estado_id) REFERENCES estado (id)
+ ,CONSTRAINT cidade_ativo_deve_ser_S_N CHECK (ativo IN ('S','N'))
+ ,CONSTRAINT cidade_unica UNIQUE (nome_cidade,estado_id)
+ 
+ );
+ 
+ INSERT INTO cidade (nome_cidade,estado_id) VALUES ('CURIIBA', 1);
+ 
+ SELECT * FROM cidade ;
